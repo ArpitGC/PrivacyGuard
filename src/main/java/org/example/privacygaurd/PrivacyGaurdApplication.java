828e7ae8e09dafc12fe966e6777d2f593a19caf6
Arpit Mishra <spresentskava@gmail.com>
2025-05-10 22:47:18 +0530

    First phase

---
+++ src/main/java/org/example/privacygaurd/PrivacyGaurdApplication.java
@@ -0,0 +1,13 @@
+package org.example.privacygaurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacyGaurdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivacyGaurdApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/privacygaurd/controller/UserController.java
@@ -0,0 +1,8 @@
+package org.example.privacygaurd.controller;
+
+
+import org.example.privacygaurd.model.User;
+
+
+public class UserController {
+}
