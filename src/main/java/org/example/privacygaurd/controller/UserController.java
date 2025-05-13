311666d5f5a8574d0147e328842c83fb15a5b12e
Arpit Mishra <spresentskava@gmail.com>
2025-05-13 08:36:26 +0530

    master

--- src/main/java/org/example/privacygaurd/controller/UserController.java
+++ src/main/java/org/example/privacygaurd/controller/UserController.java
@@ -1,8 +1,40 @@
-package org.example.privacygaurd.controller;
+package org.example.privacyguard.controller;
 
+import org.example.privacyguard.model.User;
+import org.example.privacyguard.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.example.privacygaurd.model.User;
+import java.util.List;
 
-
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User created = userService.createUser(user);
+        return ResponseEntity.ok(created);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
